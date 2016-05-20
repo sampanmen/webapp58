355ffb9607ee17fef53d33b0e0e11771a9089efc
Prapapornn <gib_love_you@hotmail.com>
2016-05-21 00:33:32 +0700

    เพิ่มการนัดหมาย

--- Student/Steachingschedule.php
+++ Student/Steachingschedule.php
@@ -3,7 +3,7 @@
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Teaching schedule  <button class="btn btn-circle glyphicon-plus" href="../Smodal_addAppointment.php" data-toggle="modal" data-target="#myModal"></button></h1>
+                <h1 class="page-header">ตารางแสดงการสอน <button class="btn btn-circle glyphicon-plus" href="Smodal_addAppointment.php" data-toggle="modal" data-target="#myModal"></button></h1>
             </div>
             <div>
                 <div class="col-lg-12">  
