6e52fb0cf15c1a70f01b18d1402b3ec6c147e3dc
sampanmen <sampan.sara@outlook.com>
2016-05-20 19:43:56 +0700

    connection

--- functions/connection.inc.php
+++ functions/connection.inc.php
@@ -1,1 +1,26 @@
 <?php
+
+define("DNS", "mysql:dbname=webapp58;host=db.webapp58.16mb.com");
+define("DBUSER", "webapp58");
+define("DBPASS", "123456");
+
+$con;
+
+function dbconnect() {
+    global $con;
+    if (!$con) {
+        $con = new PDO(DNS, DBUSER, DBPASS);
+        $con->query("SET NAMES UTF8");
+    }
+    return $con;
+}
+
+function testDB() {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT * FROM `user`";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute();
+
+    print_r($SQLPrepare->fetchAll());
+}
