adc105afcbf5cbe84cf29aeeef3be18a8ac169b9
Prapapornn <gib_love_you@hotmail.com>
2016-05-21 03:36:14 +0700

    d

--- js/student/schedule.js
+++ js/student/schedule.js
@@ -2,48 +2,16 @@ $(document).ready(function () {
     var url = 'http://' + window.location.host + '/workshop_validate_ajax/getProductsJSON.php';
     $.get(url, '', function (result) {
         var data = JSON.parse(result);
-        var day = [วันจันทร์,วันจันทร์];
         var htmlProduct = "";
         var i;
         for (key in data) {
             htmlProduct += "<tr>";
-            htmlProduct += "<td>"+day+"</td>";
+            htmlProduct += "<td>ju</td>";
             htmlProduct += "<td>" + data[key].product_code + "</td>";
             htmlProduct += "</tr>";
             i++;
         }
-        for (key in data) {
-            htmlProduct += "<tr>";
-            htmlProduct += "<td>วันอังคาร</td>";
-            htmlProduct += "<td>" + data[key].product_code + "</td>";
-            htmlProduct += "<td>" + data[key].product_name + "</td>";
-            htmlProduct += "<td>" + data[key].price + "</td>";
-            htmlProduct += "</tr>";
-        }
-        for (key in data) {
-            htmlProduct += "<tr>";
-            htmlProduct += "<td>วันพุธ</td>";
-            htmlProduct += "<td>" + data[key].product_code + "</td>";
-            htmlProduct += "<td>" + data[key].product_name + "</td>";
-            htmlProduct += "<td>" + data[key].price + "</td>";
-            htmlProduct += "</tr>";
-        }
-        for (key in data) {
-            htmlProduct += "<tr>";
-            htmlProduct += "<td>วันพฤหัสบดี</td>";
-            htmlProduct += "<td>" + data[key].product_code + "</td>";
-            htmlProduct += "<td>" + data[key].product_name + "</td>";
-            htmlProduct += "<td>" + data[key].price + "</td>";
-            htmlProduct += "</tr>";
-        }
-        for (key in data) {
-            htmlProduct += "<tr>";
-            htmlProduct += "<td>วันศุกร์</td>";
-            htmlProduct += "<td>" + data[key].product_code + "</td>";
-            htmlProduct += "<td>" + data[key].product_name + "</td>";
-            htmlProduct += "<td>" + data[key].price + "</td>";
-            htmlProduct += "</tr>";
-        }
+        
         $("#detailSchedule").html(htmlProduct);
     });
 
