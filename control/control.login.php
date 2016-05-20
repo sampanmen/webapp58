a5abce3c3f717cc4d8dd6537bafca84461a7c833
sampanmen <sampan.sara@outlook.com>
2016-05-20 20:45:39 +0700

    login with permission

--- control/control.login.php
+++ control/control.login.php
@@ -5,9 +5,11 @@ require_once '../model/login.php';
 $getUser = $_POST['username'];
 $getPass = $_POST['password'];
 
-if ($resLogin = login($getUser, $getPass) != FALSE) {
+if (($resLogin = login($getUser, $getPass)) != FALSE) {
     $getPermission = $resLogin['permission'];
-    
+//    echo $getPermission;
+//    print_r($resLogin);
+
     switch ($getPermission) {
         case 'student':
             header("Location: ../Student/SallSubject.php");
@@ -22,6 +24,7 @@ if ($resLogin = login($getUser, $getPass) != FALSE) {
             header("Location: ../index.php?p=permission_error");
             break;
     }
+
     //header("Location: ../");
 } else {
     header("Location: ../index.php?p=login_error");
