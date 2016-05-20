436a33df28ff911324c3d4c753f21b7ed07a14f3
thidanews <thidarat.changkaew@gmail.com>
2016-05-20 23:12:24 +0700

    แก้พาท

---
+++ Admin/ATerm.php
@@ -0,0 +1,30 @@
+<?php include '../head.php'; ?>
+<div id="wrapper">
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="dataTable_wrapper">
+                            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                    <tr>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                    </tr>     
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div
+</div>
+
