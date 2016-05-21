19d251988b76f0173f6d186d1164d8b75feb1188
sampanmen <sampan.sara@outlook.com>
2016-05-21 23:55:07 +0700

    edit addClass

--- control/control.admin.addRoom.php
+++ control/control.admin.addRoom.php
@@ -4,8 +4,10 @@ require_once '../model/class.php';
 
 $get_nameroom = isset($_POST['nameroom']) ? $_POST['nameroom'] : "";
 $get_teacherID = isset($_POST['teacherid']) ? $_POST['teacherid'] : "";
+$get_year = isset($_POST['year']) ? $_POST['year'] : "";
 
-if (($res = addClass($get_nameroom, $get_teacherID)) != FALSE) {
+
+if (($res = addClass($get_nameroom, $get_teacherID, $get_year)) != FALSE) {
     header("Location: ../Admin/ARoom.php?p=add_room_completed");
 } else {
     header("Location: ../Admin/ARoom.php?p=add_room_error");
