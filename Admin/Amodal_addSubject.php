0a451401b3993ccdd0bf5525b6a226b26d4ee133
sampanmen <sampan.sara@outlook.com>
2016-05-21 22:25:44 +0700

    edit addSubject ui

--- Admin/Amodal_addSubject.php
+++ Admin/Amodal_addSubject.php
@@ -1,4 +1,4 @@
-<form action="../control/control.admin.addSubject.php" method="POST">
+<form action="../control/control.admin.teaching.php" method="POST">
     <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="gridSystemModalLabel">เพิ่มวิชาใหม่</h4>
