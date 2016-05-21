b4cd93fd7926ea4bec71ec1da02c53d711f50e09
thidanews <thidarat.changkaew@gmail.com>
2016-05-21 18:00:20 +0700

    AllSubject

---
+++ Admin/AviewAllSubject.php
@@ -0,0 +1,41 @@
+<?php include '../head.php'; ?>
+<div id="wrapper">
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">All Subject <button class="btn btn-circle glyphicon-plus" href="Amodal_addAllSubject.php" data-toggle="modal" data-target="#myModal"></button></h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="dataTable_wrapper">
+                            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">No.</th>
+                                        <th>ID Subject</th>
+                                        <th>Subject Name</th>
+                                        <th>Detail</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-center">1</td>
+                                        <td>02204445</td>
+                                        <td>Web App</td>
+                                        <td><button class="btn btn-circle glyphicon-pencil" href="Amodal_editAllSubject.php" data-toggle="modal" data-target="#myModal2"></button></td>
+                                    </tr>     
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
