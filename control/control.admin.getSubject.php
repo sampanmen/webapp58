06d1273d833ed76c0b5823e6cb228710adb11006
sampanmen <sampan.sara@outlook.com>
2016-05-21 23:47:29 +0700

    used getCurrentTerm

--- control/control.admin.getSubject.php
+++ control/control.admin.getSubject.php
@@ -1,18 +1,21 @@
 <?php
 
 /*
- * @param studentid return student's subject
- * @param teacherid return teacher's subject
+ * @param studentid, term and year return student's subject
+ * @param teacherid, term and year return teacher's subject
  * @param term and year only return all for admin
  * no param return all subject
  */
 
 require_once '../model/subject.php';
+require_once '../model/term.php';
 
 $getIdStudent = isset($_REQUEST['studentid']) ? $_REQUEST['studentid'] : "";
 $getIdTeacher = isset($_REQUEST['teacherid']) ? $_REQUEST['teacherid'] : "";
-$getTerm = isset($_REQUEST['term']) ? $_REQUEST['term'] : "";
-$getYear = isset($_REQUEST['year']) ? $_REQUEST['year'] : "";
+
+$getTermDB = getCurrentTerm();
+$getTerm = isset($_REQUEST['term']) ? $_REQUEST['term'] : $getTermDB['term'];
+$getYear = isset($_REQUEST['year']) ? $_REQUEST['year'] : $getTermDB['yearTerm'];
 
 if ($getIdStudent != "") {
     $getSubject = getAllSubjectByStudent($getIdStudent, $getTerm, $getYear);
