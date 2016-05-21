4bcb34b3cc6da64c0cd6a26cf51d30c481a53d3f
sampanmen <sampan.sara@outlook.com>
2016-05-22 03:42:26 +0700

    edit student

--- control/control.admin.editStudent.php
+++ control/control.admin.editStudent.php
@@ -2,15 +2,16 @@
 
 require_once '../model/user.php';
 
+$roomid = isset($_POST['roomid']) ? $_POST['roomid'] : "";
 $idUser = isset($_POST['userid']) ? $_POST['userid'] : "";
 $titleName = isset($_POST['titlename']) ? $_POST['titlename'] : "";
 $name = isset($_POST['name']) ? $_POST['name'] : "";
 $surname = isset($_POST['sname']) ? $_POST['sname'] : "";
 $position = isset($_POST['position']) ? $_POST['position'] : "";
-$status = isset($_POST['statusteacher']) ? $_POST['statusteacher'] : "";
+$status = isset($_POST['status']) ? $_POST['status'] : "";
 
 if (updateUserInfo($idUser, $titleName, $name, $surname, $position, $status) != FALSE) {
-    header("Location: ../Admin/AviewTeacher.php?p=edit_teacher_completed");
+    header("Location: ../Admin/AviewStudent.php?roomid=$roomid&p=edit_student_completed");
 } else {
-    header("Location: ../Admin/AviewTeacher.php?p=edit_teacher_error");
+    header("Location: ../Admin/AviewStudent.php?roomid=$roomid&p=edit_student_error");
 }
