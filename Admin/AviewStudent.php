efca5f5493fcf3599d43faf47fb32c3f5f7b1629
Prapapornn <gib_love_you@hotmail.com>
2016-05-22 07:41:40 +0700

    ไทย

--- Admin/AviewStudent.php
+++ Admin/AviewStudent.php
@@ -3,7 +3,7 @@
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 id="title" class="page-header"> <button class="btn btn-circle glyphicon-plus" href="Amodal_addStudent.php?roomid=<?= $_GET['roomid']; ?>" data-toggle="modal" data-target="#myModal"></button></h1>
+                <h1 id="title" class="page-header">นักเรียน <button class="btn btn-circle glyphicon-plus" href="Amodal_addStudent.php?roomid=<?= $_GET['roomid']; ?>" data-toggle="modal" data-target="#myModal"></button></h1>
             </div>
         </div>
         <div class="row">
@@ -14,14 +14,14 @@
                             <table class="table table-striped table-bordered table-hover" id="dataTables-student">
                                 <thead>
                                     <tr>
-                                        <th class="text-center">No.</th>
-                                        <th>ID Student</th>
-                                        <th>Title Name</th>
-                                        <th>Student Name</th>
-                                        <th>Student Surname</th>
-                                        <th>Username</th>
-                                        <th>Status</th>
-                                        <th></th>
+                                        <th class="text-center">ลำดับที่</th>
+                                        <th>รหัสประจำตัว</th>
+                                        <th>คำนำหน้า</th>
+                                        <th>ชื่อ</th>
+                                        <th>นามสกุล</th>
+                                        <th>บัญชีผู้ใช้</th>
+                                        <th>สถานะ</th>
+                                        <th>ตัวเลือก</th>
                                     </tr>
                                 </thead>
                                 <tbody id="tbody">
